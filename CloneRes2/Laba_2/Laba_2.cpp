341df330db2d761481ba0c5efe77bc3f3d42f58e
yarikbely <159477291+yarikbely@users.noreply.github.com>
2024-02-21 20:21:41 +0200

    Add files via upload

---
+++ CloneRes2/Laba_2/Laba_2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+    unsigned char x, y;
+    short i;
+    int x1, result;
+    x = 98;
+
+    setlocale(LC_ALL, "Rus");
+
+    cout << "введите индекс i го бита: \n";
+    cin >> i;
+
+    y = int(pow(2, i));
+    x1 = x & y;
+    result = x1 / (pow(2, i));
+
+    cout << result;
+}
